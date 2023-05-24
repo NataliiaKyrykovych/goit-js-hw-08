83cd47df0d010e53eabca97b2358e287e7cf03f2
NataliiaKyrykovych <120452030+NataliiaKyrykovych@users.noreply.github.com>
2023-05-24 17:25:58 -0400

    new commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,54 @@
-import throttle from 'lodash.throttle';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const LOCAL_KEY = 'feedback-form-state';
+document.body.style.backgroundColor = '#f7eff4';
+const form = document.querySelector('form.form');
+const options = {
+  position: 'center-bottom',
+  distance: '15px',
+  borderRadius: '15px',
+  timeout: 10000,
+  clickToClose: true,
+  cssAnimationStyle: 'from-right',
+};
 
-form = document.querySelector('.feedback-form');
+form.addEventListener('click', onPromiseCreate);
 
-form.addEventListener('input', throttle(onInputData, 500));
-form.addEventListener('submit', onFormSubmit);
-
-let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
-const { email, message } = form.elements;
-reloadPage();
-
-function onInputData(e) {
-  dataForm = { email: email.value, message: message.value };
-  localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
-function reloadPage() {
-  if (dataForm) {
-    email.value = dataForm.email || '';
-    message.value = dataForm.message || '';
-  }
-}
-
-function onFormSubmit(e) {
+function onPromiseCreate(e) {
   e.preventDefault();
-  console.log({ email: email.value, message: message.value });
+  const { delay, step, amount } = e.currentTarget.elements;
+  let inputDelay = Number(delay.value);
+  let inputStep = Number(step.value);
+  let inputAmount = Number(amount.value);
 
-  if (email.value === '' || message.value === '') {
-    return alert('Please fill in all the fields!');
+  for (let i = 1; i <= inputAmount; i += 1) {
+    inputDelay += inputStep;
+
+    createPromise(i, inputDelay)
+      .then(({ position, delay }) => {
+        Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          options
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          options
+        );
+      });
+    e.currentTarget.reset();
   }
-
-  localStorage.removeItem(LOCAL_KEY);
-  e.currentTarget.reset();
-  dataForm = {};
 }
